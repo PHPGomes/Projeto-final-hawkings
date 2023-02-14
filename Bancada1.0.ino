0d55681b8d553eddd200201e5ec1c6e0e002114f
Otavio Fonseca <otavioaf123@gmail.com>
2023-02-14 11:27:45 -0300

    Ajuste variavel

--- Bancada1.0.ino
+++ Bancada1.0.ino
@@ -38,7 +38,7 @@ void loop() {
     lcd.print(correnteFinal); // imprime a corrente na tela
 
     lcd.print("W:");
-    lcd.print("medsc");
+    lcd.print(medsc);
 
     lcd.setCursor(0,1);//muda para linha inferior do lcd
     
