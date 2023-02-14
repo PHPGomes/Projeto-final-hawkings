2abe234547f2b42dae3a4c6355ce5a4a91ffa6bb
augustofrazao <augustofrazao1@gmail.com>
2023-02-14 12:15:18 -0300

    Update Bancada1.0.ino

--- Bancada1.0.ino
+++ Bancada1.0.ino
@@ -3,9 +3,9 @@
 #include <LiquidCrystal.h> //Biblioteca para o LCD
 
 float medsc;
-
+float tensao;
 #define correnteCalibracao 18.40 // Valor de Calibração do Sensor 
-
+const int RPMV = 1000 // Constante RMP/V do motor. Valor exemplo tem que ver com a mecanica o RPM/V do motor
 const int pinoSensor = A2; // Entrada Analógica 2 do Arduino
 EnergyMonitor medidor; // Declaração de variável da Classe EnergyMonitor
 
@@ -20,16 +20,15 @@ void setup() {
 
     lcd.begin(16, 2);//proporcao do lcd em linhas e colunas respectivamente(inicia o lcd)
 
-
-
 }
 
 void loop() {
     medsc = DFRobot_HX711_I2C.readWheight(6); // define medsc como a leitura do sensor de carga
-   
     medidor.calcVI(20,200) // Calcula a Potência (V*I) (20 semiciclos / tempo limite para fazer a medição)
     double correnteFinal = medidor.Irms; // A variável "correnteFinal" recebe o valor da corrente em RMS
-
+    tensao = medirTensao(); // Mede a tensão do sistema. De a cordo com o pedro tem jeito do arduino ler a resistencia do potenciometro,
+                            // se nao tiver sempre tem como fazer uma regra de três
+    
     /*Serial.print("Corrente Medida: ");
     Serial.print(correnteFinal);
     Serial.print("A");*/
@@ -37,9 +36,11 @@ void loop() {
     lcd.print("A: ");
     lcd.print(correnteFinal); // imprime a corrente na tela
 
-    lcd.print("W:");
+    lcd.print("W: ");
     lcd.print(medsc);
 
     lcd.setCursor(0,1);//muda para linha inferior do lcd
     
+    lcd.print("RPM: ");
+    lcd.print(RPMV*tensao)
 }
