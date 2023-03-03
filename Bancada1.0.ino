661fab45779d6108fdbdf0911c97d03d4252d36d
Otavio Fonseca <90848245+ootaviofonseca@users.noreply.github.com>
2023-03-02 22:13:20 -0300

    Update Bancada1.0.ino

--- Bancada1.0.ino
+++ Bancada1.0.ino
@@ -1,35 +1,53 @@
 #include <DFRobot_HX711_I2C.h>
 #include <EmonLib.h>
-#include <LiquidCrystal.h> //Biblioteca para o LCD
+
+#include "HX711.h"
+
+#include <Wire.h> // Biblioteca utilizada para fazer a comunicação com o I2C
+#include <LiquidCrystal_I2C.h>
 
 float medsc;
 float tensao;
 float Porcentagem_Potenciometro;
-#define correnteCalibracao 18.40 // Valor de Calibração do Sensor 
-#define potenciometro A0
-#define esc 5
-const int RPMV = 1000 // Constante RMP/V do motor. Valor exemplo tem que ver com a mecanica o RPM/V do motor
+float correnteCalibracao = 18.40; // Valor de Calibração do Sensor 
+int esc = 5;
+const int RPMV = 1000;// Constante RMP/V do motor. Valor exemplo tem que ver com a mecanica o RPM/V do motor
 const int pinoSensor = A2; // Entrada Analógica 2 do Arduino
 EnergyMonitor medidor; // Declaração de variável da Classe EnergyMonitor
 
-LiquidCrystal lcd(11, 9, 4, 5, 6, 13);//definição das portas de conexao do lcd 
+#define DT A2
+#define SCK A1
+
+HX711 escala;
+
+LiquidCrystal_I2C lcd(0x3f , 16, 2); // Chamada da funcação LiquidCrystal para ser usada com o I2C
+
 //Portas na Documentacao
 void setup() {
-    DFRobot_HX711_I2C.begin(); //inicia o sensor de carga
-    DFRobot_HX711_I2C.getCalibration(); //calibra o sensor de carga
+    Serial.begin(9600);// Define a velocidade da transmissão de dados em bits por segundos
+    Wire.begin();
 
-    Serial.begin(9600); // Define a velocidade da transmissão de dados em bits por segundos
+    escala.begin (DT, SCK);
+    Serial.println(escala.read());
+    escala.set_scale(5555556.55); 
+
+    escala.tare(0);
+
     medidor.current(pinoSensor,correnteCalibracao); // Passa os parâmetros (Pino Analógico / Valor de Calibração)
-
-    lcd.begin(16, 2);//proporcao do lcd em linhas e colunas respectivamente(inicia o lcd)
-
-    pinMode(potenciometro, INPUT);
+   
+    /*lcd.init(); //SETA A QUANTIDADE DE COLUNAS(16) E O NÚMERO DE LINHAS(2) DO DISPLAY
+    //lcd.setBacklight(HIGH); // Serve para iniciar a comunicação com o display já conectado
+    
+    lcd.backlight(); // Serve para ligar a luz do display
+    //lcd nao esta funcionando usando o serial print mesmo
+    */
+    pinMode(A0, INPUT);
     pinMode(esc,OUTPUT);
 
 }
 
 void potenciometro_esc(){
-    int Nivel_potenciometro = analogRead(potenciometro); // leitura analogica potenciômetro
+    int Nivel_potenciometro = analogRead(A0); // leitura analogica potenciômetro
     int Nivel_esc = Nivel_potenciometro * 0.1749755620; //conversor do potenciometro para o esc (valor entre 0 e 1023, para um valor entre 0 e 179)
     Porcentagem_Potenciometro = (Nivel_potenciometro/1023)*100; //Porcentagem de acionamento do potenciômetro
     analogWrite(esc,Nivel_esc); // acionamento esc
@@ -37,29 +55,27 @@ void potenciometro_esc(){
 
 void loop() {
     
-
-    medsc = DFRobot_HX711_I2C.readWheight(6); // define medsc como a leitura do sensor de carga
+    medsc = escala.get_value(1);// define medsc como a leitura do sensor de carga
     //medidor.calcVI(20,200) // Calcula a Potência (V*I) (20 semiciclos / tempo limite para fazer a medição)
     double correnteFinal = medidor.Irms; // A variável "correnteFinal" recebe o valor da corrente em RMS                    
     potenciometro_esc();
-    tensao = Porcentagem_Potenciometro * 200;// Mudar valor da resistencia
+    tensao = Porcentagem_Potenciometro * 10000;// Mudar valor da resistencia
     
-    lcd.print("A: ");
-    lcd.print(correnteFinal); // imprime a corrente na tela
+    Serial.print("A: ");
+    Serial.println(correnteFinal); // imprime a corrente na tela
 
-    lcd.print("W: ");
-    lcd.print(medsc);//Sensor de Carga
+    Serial.print("W: ");
+    Serial.println(medsc);//Sensor de Carga
 
-    lcd.setCursor(0,1);//muda para linha inferior do lcd
+    //Serial.setCursor(0,1);//muda para linha inferior do lcd
     
-    lcd.print("RPM: ");
-    lcd.print(RPMV*tensao)
+    Serial.print("RPM: ");
+    Serial.println(RPMV*tensao);
 
+    ///pORCENTAGEM DO POTENCIOMETRO
+    Serial.print((analogRead(A0)/1023.0)*100);
+    Serial.println("%: ");
     
-    lcd.print(Porcentagem_Potenciometro);
-    lcd.print("%: ");
-    
-
-    delay(2000);//Ajustar delay ideal
-
+    delay(100);//Ajustar delay ideal
+    Serial.println("\n\n");
 }
